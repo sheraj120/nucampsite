3a4765560dab6d516411f77aa30e0de8bba89c13
Md Islam <sheraj120@yahoo.com>
2021-04-17 12:58:59 -0700

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -12,5 +12,10 @@ $(function(){
       }
       
   });
-  
+  $("#reserveButton").click(function(){
+      $("#reserveModal").modal("show");
+  });
+  $("#loginButton").click(function(){
+    $("#loginModal").modal("show");
+});
 });
